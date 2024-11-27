de1e5d30ad8905b4775b5b5071d18dbd57b3bb74
Matt Yeldalo <matman370@hotmail.com>
2024-11-27 21:56:45 +1100

    wip: parse of args

--- lua/quick-note/init.lua
+++ lua/quick-note/init.lua
@@ -1,15 +1,41 @@
 local M = {}
 
+-- opts = {'action', 'name'}
 M.quickNote = function(opts)
-	local args = vim.split(opts.args, " ")
-	print("Your args " .. vim.inspect(args))
+	print("Your opts are: " .. vim.inspect(opts))
+	local len = #opts
+
+	print(opts[0])
+
+	if opts == nil or opts[0] == nil or len < 1 then
+		print("Creating new QuickNote project.")
+		CreateNew()
+		return
+	end
+
+	if len < 2 then
+		print("Opts are 1")
+		ParseAction(opts[0])
+		return
+	end
+
+	print(opts)
 end
 
-vim.api.nvim_create_user_command(
-	"QuickNote",
-	function(opts)
-		M.quickNote(opts.args)
-	end,
-	{ nargs = "*" })
+function CreateNew()
+	print("CreateNew")
+end
+
+function ParseAction(action)
+	if action == "open" then
+		print("open")
+	elseif action == "new" then
+		print("new")
+	end
+end
+
+vim.api.nvim_create_user_command("QuickNote", function(opts)
+	M.quickNote(vim.split(opts.args, " "))
+end, { nargs = "*" })
 
 return M
